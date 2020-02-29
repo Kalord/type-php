9b43b524a4d82fed6e09e9e18cf94b322adfe64d
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-02-29 18:55:30 +0800

    Added Print

---
+++ src/logger/Print.cpp
@@ -0,0 +1,20 @@
+#include "Print.hpp"
+
+void Print::allLogs(Logger* logger)
+{
+    auto records = logger->getRecords();
+
+    if(logger->existsError())
+    {
+        std::cout << "Errors: " << logger->countError() << std::endl;
+    }
+
+    int step = 1;
+    for(auto record : records)
+    {
+        std::cout << "Step: " << step << record.getContent() << std::endl;
+        if(record.getType() == SUCCESS) std::cout << "Success" << std::endl;
+        if(record.getType() == ERROR) std::cout << "Error" << std::endl;
+        step++;
+    }
+}
---
+++ src/logger/Print.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+
+#include "Logger.hpp"
+
+class Print
+{
+public:
+    static void allLogs(Logger* logger);
+}
