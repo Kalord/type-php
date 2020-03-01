dd712e4ef8b3cb9df28825016df47725ac2dcf70
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-01 14:48:21 +0800

    Refactoring

--- src/io/Input.cpp
+++ src/io/Input.cpp
@@ -5,23 +5,35 @@
  **/
 void Input::processingRequest(int argv, char** args)
 {
-    TranspilerList transpilerList;
+    if(DEBUG)
+    {
+        Debug::printMessage("In method processingRequest");
+    }
+
     Make make;
 
     //Use make
     if(argv == 2 && args[1] == "make")
     {
-        transpilerList = make.getTranspilerList();
+        make.parseConfig();
     }
     else if(argv == 3 && args[1] == "make")
     {
-        //make.setPathToConfig(args[2]);
-        transpilerList = make.getTranspilerList();
+        make.setPathToConfig(args[2]);
+        make.parseConfig();
     }
     else if(argv > 1)
     {
+        TranspilerList transpilerList;
         for(int i = 1; i < argv; i++) transpilerList.add(args[i]);
-        //make.loadTranspilerList(transpilerList);
+        make.loadTranspilerList(transpilerList);
+        make.parseConfig();
+    }
+
+    
+    if(DEBUG)
+    {
+        Debug::printMessage("End method processingRequest");
     }
 
     //(Transpiler(make.getTranspilerList(), make.getVersionPHP(), make.getOutput()));
